070ed629114f1b9ea78c6138f6b4a14475cc7ecb
b-4321 <bishwadeepkhanra4321@gmail.com>
2025-08-29 15:06:14 +0530

    Add password management with localStorage support
    Implemented state management for site, username, and password fields. Added functionality to save passwords to localStorage and display them in a table. Enhanced the UI to handle form input and show saved passwords.

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,49 +1,142 @@
-import React, { use } from 'react'
-import { useRef } from 'react'
+import React, { use, useState } from "react";
+import { useRef, useEffect } from "react";
 
 const Manager = () => {
-    const ref = useRef()
-    const showPassword = () => {
-        alert('show password');
-        if(ref.current.src.includes("/eye-block.png")){
-            ref.current.src = "/eye-open.png";
-        } else {
-            ref.current.src = "/eye-block.png";
-        }
+  const ref = useRef();
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setpasswordArray] = useState([]);
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    if (passwords) {
+      setpasswordArray(JSON.parse(passwords));
     }
+  }, []);
+
+  const showPassword = () => {
+    alert("show password");
+    if (ref.current.src.includes("/eye-block.png")) {
+      ref.current.src = "/eye-open.png";
+    } else {
+      ref.current.src = "/eye-block.png";
+    }
+  };
+
+  const savePassword = () => {
+    setpasswordArray([...passwordArray, form]);
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
+    console.log([...passwordArray, form]);
+  };
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
-     <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
-    <div className='bg-yellow-200 mycontainer rounded-lg h-96 my-16'>
-        <h1 className='text-4xl font-bold text- text-center'>
-            <span className='text-red-700'> &lt; </span>
-            <span>Pass</span>
-            <span className='text-red-700'>OP/&gt; </span>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+      <div className="bg-yellow-200 mycontainer rounded-lg  my-2">
+        <h1 className="text-4xl font-bold text- text-center">
+          <span className="text-red-700"> &lt; </span>
+          <span>Pass</span>
+          <span className="text-red-700">OP/&gt; </span>
         </h1>
-        <p className='text-center text-red-900 text-lg'>Your one-stop solution for password management.</p>
-        <div className='text-white flex flex-col p-4 text-black gap-8 items-center'>
-            <input placeholder='Enter website name' className='rounded-full border border-red-700 w-full p-4 py-1' type="text" name="" id="" />
-            <div className='flex w-full justify-between gap-3'>
-                <input placeholder='Enter username' type="text" className='rounded-full border border-red-700 w-full p-4 py-1'/>
-                <div className="relative">
-                <input placeholder='Enter password' type="text" className='rounded-full border border-red-700 w-full p-4 py-1'/>
-                <span className='absolute right-[4px] top-[4px] cursor-pointer 'onClick={showPassword}>
-                    <img ref={ref} className='p-1' width={30} src="/eye-open.png" alt="eye" />
-                </span>
-                </div>
+        <p className="text-center text-red-900 text-lg">
+          Your one-stop solution for password management.
+        </p>
+        <div className="text-white flex flex-col p-4 text-black gap-8 items-center">
+          <input
+            value={form.site}
+            onChange={handleChange}
+            placeholder="Enter website name"
+            className="rounded-full border border-red-700 w-full p-4 py-1"
+            type="text"
+            name="site"
+            id=""
+          />
+
+          <div className="flex w-full justify-between gap-3">
+            <input
+              value={form.username}
+              onChange={handleChange}
+              placeholder="Enter username"
+              type="text"
+              className="rounded-full border border-red-700 w-full p-4 py-1"
+              name="username"
+              id=""
+            />
+            <div className="relative">
+              <input
+                value={form.password}
+                onChange={handleChange}
+                placeholder="Enter password"
+                type="text"
+                className="rounded-full border border-red-700 w-full p-4 py-1"
+                name="password"
+                id=""
+              />
+              <span
+                className="absolute right-[4px] top-[4px] cursor-pointer "
+                onClick={showPassword}
+              >
+                <img
+                  ref={ref}
+                  className="p-1"
+                  width={30}
+                  src="/eye-open.png"
+                  alt="eye"
+                />
+              </span>
             </div>
+          </div>
 
-            <button className='flex justify-center items-center gap-2 bg-red-600 hover:bg-red-500 rounded-full px-8 py-2 w-fit border-2 border-red-700'>
-                <lord-icon
-            src="https://cdn.lordicon.com/vjgknpfx.json"
-            trigger="hover">
-            </lord-icon>
-                Add Password</button>
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center gap-2 bg-red-600 hover:bg-red-500 rounded-full px-8 py-2 w-fit border-2 border-red-700"
+          >
+            <lord-icon
+              src="https://cdn.lordicon.com/vjgknpfx.json"
+              trigger="hover"
+            ></lord-icon>
+            Add Password
+          </button>
         </div>
-    </div>
-     
-    </>
-  )
-}
 
-export default Manager
+        <div className="passwords">
+          <h2 className="font-bold text-2xl py-2">Your Saved Passwords</h2>
+          {passwordArray.length === 0 && <div>No passwords saved yet.</div>}
+          {passwordArray.length !== 0 && <table className="table-auto w-full rounded-md overflow-hidden">
+            <thead className="bg-red-800">
+              <tr>
+                <th className="py-2">site</th>
+                <th className="py-2">username</th>
+                <th className="py-2">password</th>
+              </tr>
+            </thead>
+            <tbody className="bg-red-100">
+              {passwordArray.map((item,index) => {
+                return (
+                  <tr key={index}>
+                    <td className="py-2 border border-white text-center w-32">
+                      <a href={item.site}target="_blank">{item.site}</a>
+                    </td>
+                    <td className="py-2 border border-white text-center w-32">
+                      {item.username}
+                    </td>
+                    <td className="py-2 border border-white text-center w-32">
+                      {item.password}
+                    </td>
+                  </tr>
+                );
+               
+              })}
+              
+            </tbody>
+          </table>}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Manager;
