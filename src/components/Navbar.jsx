052c21b40154f31bcbcac80a5fa88b0a1cd024bf
b-4321 <bishwadeepkhanra4321@gmail.com>
2025-09-04 11:09:08 +0530

    Update GitHub repo link in Navbar component
    Changed the GitHub button link in the Navbar from 'https://github.com/your-repo' to 'https://github.com/b-4321/Password' for accuracy. Also reformatted JSX for improved readability.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,42 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-purple-200 rounded-lg my-1 mx-1'>
-        <div className='flex justify-around items-center px-4 py-5 h-16 '>
-
-        <div className='logo font-bold text-2xl'>
-            <span className='text-red-700'> &lt; </span>
-            Pass
-            <span className='text-red-700'>OP/&gt; </span>
-            </div>
-     {/* <ul>
+    <nav className="bg-purple-200 rounded-lg my-1 mx-1">
+      <div className="flex justify-around items-center px-4 py-5 h-16 ">
+        <div className="logo font-bold text-2xl">
+          <span className="text-red-700"> &lt; </span>
+          Pass
+          <span className="text-red-700">OP/&gt; </span>
+        </div>
+        {/* <ul>
         <li className='flex gap-4'>
             <a className='hover:font-bold' href="/">Home</a>
             <a className='hover:font-bold' href="#">About</a>
             <a className='hover:font-bold' href="#">Contact</a>
         </li>
       </ul> */}
-      <button className='flex items-center'>
-        <img className='cursor-pointer p-1' width={30} src="github.png" alt="GitHub Logo" />
-        <span className='font-bold' onClick={() => window.open("https://github.com/your-repo", "_blank")}>Github</span>
-      </button>
-        </div>
+        <button className="flex items-center">
+          <img
+            className="cursor-pointer p-1"
+            width={30}
+            src="github.png"
+            alt="GitHub Logo"
+          />
+          <span
+            className="font-bold"
+            onClick={() =>
+              window.open(
+                "https://github.com/b-4321/Password",
+                "_blank"
+              )
+            }
+          >
+            Github
+          </span>
+        </button>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar
