76a03934d5a4d264a4df5b22b20cb9a7b0e00deb
b-4321 <bishwadeepkhanra4321@gmail.com>
2025-09-04 11:13:08 +0530

    Make footer fixed to bottom of the page
    Added Tailwind CSS classes 'fixed bottom-0 left-0 right-0' to the footer container to ensure it stays at the bottom of the viewport.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-gray-800 text-white flex flex-col justify-center items-center py-2 mx-1 my-1 rounded-lg">
+    <div className="bg-gray-800 text-white flex flex-col justify-center items-center py-2 mx-1 my-1 rounded-lg fixed bottom-0 left-0 right-0">
       <div className="logo font-bold text-1xl">
         <span className="text-red-700"> &lt; </span>
         Pass
